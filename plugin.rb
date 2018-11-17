c8cfe5cdf3f2e748fd322a9ade9fdbbde61bc9d5
SigmundurMorkore <sigmundurmorkore@gmail.com>
2018-11-17 18:45:54 +0000

    Renamed to fit naming standard

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
-# name: DiscoursePreventModIP
+# name: discourse-prevent-mod-ip
 # about: A simple plugin to replace the user-index.hbs template, so mods don't have access to users' IPs
-# version: 1
+# version: 1.0.1
 # authors: SigmundurMorkore
 # url: https://github.com/
 
